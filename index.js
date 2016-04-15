b10a59b6a0933305173613a36d2e5d1e6c9858de
n0bisuke <rsksound@gmail.com>
2016-04-15 11:02:18 +0900

    add bot

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.set('port', (process.env.PORT || 5000));
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+//
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+//
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+const LineBot = require('@3846masa/linebot');
+const bot = new LineBot({
+  channelID: '1462882381',
+  channelSecret: 'f8529202cefd073d173d51dbd445b73c',
+  MID: 'u356bf0e1d9c5143a4de80e036cc9e222'
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+bot.on('message', (res) => {
+  const content = res.content;
+  if ( content.contentType === LineBot.CONST.CONTENT_TYPE.TEXT ) {
+    bot.postText({
+      user: content.from,
+      message: content.text
+    });
+  } else {
+    bot.postText({
+      user: content.from,
+      message: 'Not text.'
+    });
+  }
 });
 
-
+bot.listen(5000);
